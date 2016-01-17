e18909d1c40f8af9968bf2717fcd6875fdc2bc87
Jordan Shute <jordan.shute@icloud.com>
2016-01-17 10:13:50 -0700

    Admin slots controller works with framework again

--- controllers/slots.php
+++ controllers/slots.php
@@ -8,21 +8,25 @@ class slots extends controller {
 			sq::controller('auth')->action($action);
 		}
 		
-		if (!$this->options['require-login'] || auth::check('admin')) {
+		if (!$this->options['require-login'] || sq::auth()->level == 'admin') {
 			return true;
 		} else {
 			return sq::view('admin/login');
 		}
 	}
 	
+	public function init() {
+		$this->layout->modelName = 'slots';
+	}
+	
 	public function indexAction() {
-		$this->layout->content = sq::model('sq_slots')
-			->read();
+		sq::load('/defaults/slot');
+		$this->layout->content = sq::model('sq_slots')->read();
 	}
 	
 	public function updateAction($id) {
-		$slot = sq::model('sq_slots', array('layout' => 'admin/slots/edit'))
-			->find($id);
+		$slot = sq::model('sq_slots')->find($id);
+		$slot->layout = sq::view('admin/slots/edit');
 		
 		if (sq::request()->isPost) {
 			$content = sq::request()->post('content');
