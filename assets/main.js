e72008b04ae51d3ad56b8a904566fe848c34285d
Jordan Shute <jordan.shute@icloud.com>
2018-07-23 20:20:28 -0700

    Change admin rich text editor to trumbowyg

--- assets/main.js
+++ assets/main.js
@@ -22,18 +22,18 @@ $(function() {
 		});
 	}
 
-	$("textarea.richtext").tinymce({
-		script_url: tinymcePath + "/tinymce.min.js",
-		statusbar: false,
-		body_id: "mce-content",
-		content_css: "../../assets/admin/main.css?" + new Date().getTime(),
-		plugins: [
-			"advlist autolink lists link image charmap print preview anchor",
-			"searchreplace visualblocks code fullscreen",
-			"insertdatetime media table contextmenu paste"
+	$('.sq-rich-editor').trumbowyg({
+		btns: [
+			['viewHTML'],
+        	['undo', 'redo'],
+        	['formatting'],
+        	['strong', 'em'],
+        	['link'],
+        	['unorderedList', 'orderedList'],
+        	['horizontalRule'],
+        	['removeformat'],
 		],
-
-		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+		autogrow: true
 	});
 
 	var active = false;
--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,21 +1,29 @@
 <?
 
-$tinymce = sq::asset('admin/tinymce');
-
+// Add id to body
 self::$id = 'admin';
+
+// jQuery
+self::script(sq::asset('admin/vendor/jquery/jquery-3.3.1.min.js'));
+sq::asset('admin/vendor/jquery/jquery-3.3.1.min.map');
+
+// Drag and drop sorting
+self::script(sq::asset('admin/vendor/html5sortable/html5sortable.min.js'));
+sq::asset('admin/vendor/html5sortable/html5sortable.min.js.map');
+
+// Editor
+sq::asset('admin/vendor/trumbowyg');
+self::script(sq::asset('admin/vendor/trumbowyg/trumbowyg.min.js'));
+self::style(sq::asset('admin/vendor/trumbowyg/ui/trumbowyg.min.css'));
+
+// Main Javascript
+self::script(sq::asset('admin/main.js'));
+
+// Mail styles and fonts
 self::style('http://fonts.googleapis.com/css?family=Open+Sans:300italic,400,700');
 self::style(sq::asset('admin/main.css'));
 
-self::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
-self::script($tinymce.'/jquery.tinymce.min.js');
-self::script(sq::asset('admin/vendor/html5sortable.min.js'));
-sq::asset('admin/vendor/html5sortable.min.js.map');
-self::script(sq::asset('admin/main.js'));
-
+// Meta
 self::$favicon = sq::asset('admin/favicon.ico');
 self::$head = '<meta name="viewport" content="initial-scale=1.0"/>';
-
 self::$title = sq::config('admin/title');
-
-?>
-<script>var tinymcePath = '<?=$tinymce ?>';</script>
